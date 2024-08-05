cffc9fd37ff7f0e42b26c97c93f14b33b1a70c91
fatihkurtl <kurtf061@gmail.com>
2024-08-05 17:28:53 +0300

    invoices table edited

--- resources/views/admin/invoices.blade.php
+++ resources/views/admin/invoices.blade.php
@@ -24,7 +24,7 @@
                         <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                             <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
                         </svg>
-                        Add product
+                        Excel'e aktar
                     </button>
                     <div class="flex items-center space-x-3 w-full md:w-auto">
                         <button id="actionsDropdownButton" data-dropdown-toggle="actionsDropdown" class="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" type="button">
@@ -84,11 +84,12 @@
                 <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
-                            <th scope="col" class="px-4 py-3">Product name</th>
-                            <th scope="col" class="px-4 py-3">Category</th>
-                            <th scope="col" class="px-4 py-3">Brand</th>
-                            <th scope="col" class="px-4 py-3">Description</th>
-                            <th scope="col" class="px-4 py-3">Price</th>
+                            <th scope="col" class="px-4 py-3">Fatura</th>
+                            <th scope="col" class="px-4 py-3">Alıcı</th>
+                            <th scope="col" class="px-4 py-3">Durum</th>
+                            <th scope="col" class="px-4 py-3">Ödeme</th>
+                            <th scope="col" class="px-4 py-3">Tarih</th>
+                            <th scope="col" class="px-4 py-3">Toplam Fiyat</th>
                             <th scope="col" class="px-4 py-3">
                                 <span class="sr-only">Actions</span>
                             </th>
@@ -100,6 +101,7 @@
                             <td class="px-4 py-3">PC</td>
                             <td class="px-4 py-3">Apple</td>
                             <td class="px-4 py-3">300</td>
+                            <td class="px-4 py-3">$2999</td>
                             <td class="px-4 py-3">$2999</td>
                             <td class="px-4 py-3 flex items-center justify-end">
                                 <button id="apple-imac-27-dropdown-button" data-dropdown-toggle="apple-imac-27-dropdown" class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100" type="button">
@@ -128,6 +130,7 @@
                             <td class="px-4 py-3">Apple</td>
                             <td class="px-4 py-3">200</td>
                             <td class="px-4 py-3">$1499</td>
+                            <td class="px-4 py-3">$1499</td>
                             <td class="px-4 py-3 flex items-center justify-end">
                                 <button id="apple-imac-20-dropdown-button" data-dropdown-toggle="apple-imac-20-dropdown" class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100" type="button">
                                     <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -154,6 +157,7 @@
                             <td class="px-4 py-3">Phone</td>
                             <td class="px-4 py-3">Apple</td>
                             <td class="px-4 py-3">1237</td>
+                            <td class="px-4 py-3">$999</td>
                             <td class="px-4 py-3">$999</td>
                             <td class="px-4 py-3 flex items-center justify-end">
                                 <button id="apple-iphone-14-dropdown-button" data-dropdown-toggle="apple-iphone-14-dropdown" class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100" type="button">
@@ -182,6 +186,7 @@
                             <td class="px-4 py-3">Apple</td>
                             <td class="px-4 py-3">4578</td>
                             <td class="px-4 py-3">$1199</td>
+                            <td class="px-4 py-3">$1199</td>
                             <td class="px-4 py-3 flex items-center justify-end">
                                 <button id="apple-ipad-air-dropdown-button" data-dropdown-toggle="apple-ipad-air-dropdown" class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100" type="button">
                                     <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -208,6 +213,7 @@
                             <td class="px-4 py-3">Gaming/Console</td>
                             <td class="px-4 py-3">Microsoft</td>
                             <td class="px-4 py-3">56</td>
+                            <td class="px-4 py-3">$299</td>
                             <td class="px-4 py-3">$299</td>
                             <td class="px-4 py-3 flex items-center justify-end">
                                 <button id="xbox-series-s-dropdown-button" data-dropdown-toggle="xbox-series-s-dropdown" class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100" type="button">
