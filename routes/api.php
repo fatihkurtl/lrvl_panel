07b670611196567d97f64fe2517d7f52a05ab687
fatihkurtl <kurtf061@gmail.com>
2024-08-08 21:18:21 +0300

    CartController added for api

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+// Route::post('/add-to-cart', [\App\Http\Controllers\CartController::class, 'store'])->name('cart.store');
+// Route::post('/update-cart', [\App\Http\Controllers\CartController::class, 'update'])->name('cart.update');
+// Route::post('/remove-from-cart', [\App\Http\Controllers\CartController::class, 'destroy'])->name('cart.destroy');
